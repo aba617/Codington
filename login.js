7466df0c9a56d4923d1681812f13b30a884460af
Allen <allenaronis@gmail.com>
2015-03-19 15:56:45 -0500

    Login added
    Added login

---
+++ login.js
@@ -0,0 +1,7 @@
+function validateForm(){
+	var email = document.getElementById("inputEmail").value;
+	var pass = document.getElementById("inputPassword").value;
+
+	console.log(email);
+	console.log(pass);
+	}
